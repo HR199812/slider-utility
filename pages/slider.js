4ba5a3792c6f552f717bfd0cd2a675224eb22d1c
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2022-05-22 10:25:32 +0530

    App Fix

--- pages/slider.js
+++ pages/slider.js
@@ -1,89 +1,93 @@
 import React from "react";
-var $ = require("jquery");
-if (typeof window !== "undefined") {
-  // Client-side-only code
-  window.$ = window.jQuery = require("jquery");
-  var dots = window
-    .$(".owl-dots")
-    .css("position", "absolute")
-    .css("bottom", "5px");
-  dots.css("left", "calc(50% - " + dots.width() / 2 + "px)");
-}
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
-import dynamic from "next/dynamic";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, A11y, Autoplay, Mousewheel, Parallax } from "swiper";
 
-const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
-  ssr: false,
-});
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
+import "swiper/css/mousewheel";
+import "swiper/css/parallax";
 
-export default function Slider() {
-  const options = {
-    loop: true,
-    items: 1,
-    margin: 0,
-    autoplay: true,
-    animateOut: "slideOutUp",
-    animateIn: "slideInUp",
-  };
+const Slider = () => {
   return (
-    <>
-      <div className="app">
-        <OwlCarousel
-          className="owl-theme"
-          {...options}
-          style={{ transform: `rotate(-90deg)` }}
-        >
-          <div className="item">
-            <div className="item-child-container">
-              <div className="item-subchild">
-                <img
-                  src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/3/28/0328192-2590-FCBD-5A018FCFA66A0706.jpg"
-                  alt="sunrise"
-                />
-              </div>
-              <div className="item-subchild">
-                <img
-                  src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
-                  alt="sunrise"
-                />
-              </div>
-            </div>
+    <Swiper
+      // install Swiper modules
+      modules={[Pagination, A11y, Autoplay, Mousewheel, Parallax]}
+      spaceBetween={0}
+      slidesPerView={1}
+      autoplay={{ delay: 3000 }}
+      mousewheel={{ invert: true }}
+      pagination={{
+        clickable: true,
+        dynamicBullets: true,
+      }}
+      preloadImages={false}
+      loop={true}
+      watchSlidesProgress={true}
+      speed={1000}
+      parallax={true}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+      style={{ transform: `rotate(-90deg)` }}
+    >
+      <SwiperSlide>
+        <div className="item-child-container">
+          <div className="item-subchild">
+            <img
+              data-swiper-parallax={-200}
+              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/3/28/0328192-2590-FCBD-5A018FCFA66A0706.jpg"
+              alt="sunrise"
+            />
           </div>
-          <div className="item">
-            <div className="item-child-container">
-              <div className="item-subchild">
-                <img
-                  src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
-                  alt="sunrise"
-                />
-              </div>
-              <div className="item-subchild">
-                <img
-                  src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
-                  alt="sunrise"
-                />
-              </div>
-            </div>
+          <div className="item-subchild">
+            <img
+              data-swiper-parallax={-200}
+              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
+              alt="sunrise"
+            />
           </div>
-          <div className="item">
-            <div className="item-child-container">
-              <div className="item-subchild">
-                <img
-                  src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/3/28/0328192-2590-FCBD-5A018FCFA66A0706.jpg"
-                  alt="sunrise"
-                />
-              </div>
-              <div className="item-subchild">
-                <img
-                  src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
-                  alt="sunrise"
-                />
-              </div>
-            </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="item-child-container">
+          <div className="item-subchild">
+            <img
+              data-swiper-parallax={-200}
+              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/3/28/0328192-2590-FCBD-5A018FCFA66A0706.jpg"
+              alt="sunrise"
+            />
           </div>
-        </OwlCarousel>
-      </div>
-    </>
+          <div className="item-subchild">
+            <img
+              data-swiper-parallax={-200}
+              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
+              alt="sunrise"
+            />
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="item-child-container">
+          <div className="item-subchild">
+            <img
+              data-swiper-parallax={-200}
+              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/3/28/0328192-2590-FCBD-5A018FCFA66A0706.jpg"
+              alt="sunrise"
+            />
+          </div>
+          <div className="item-subchild">
+            <img
+              data-swiper-parallax={-200}
+              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
+              alt="sunrise"
+            />
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
-}
+};
+export default Slider;
