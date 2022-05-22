2d67c26639aa08245fc8105cb3f7642d9e193e06
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2022-05-22 10:36:23 +0530

    Update slider.js

--- pages/slider.js
+++ pages/slider.js
@@ -13,81 +13,83 @@ import "swiper/css/parallax";
 
 const Slider = () => {
   return (
-    <Swiper
-      // install Swiper modules
-      modules={[Pagination, A11y, Autoplay, Mousewheel, Parallax]}
-      spaceBetween={0}
-      slidesPerView={1}
-      autoplay={{ delay: 3000 }}
-      mousewheel={{ invert: true }}
-      pagination={{
-        clickable: true,
-        dynamicBullets: true,
-      }}
-      preloadImages={false}
-      loop={true}
-      watchSlidesProgress={true}
-      speed={1000}
-      parallax={true}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log("slide change")}
-      style={{ transform: `rotate(-90deg)` }}
-    >
-      <SwiperSlide>
-        <div className="item-child-container">
-          <div className="item-subchild">
-            <img
-              data-swiper-parallax={-200}
-              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/3/28/0328192-2590-FCBD-5A018FCFA66A0706.jpg"
-              alt="sunrise"
-            />
+    <>
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination, A11y, Autoplay, Mousewheel, Parallax]}
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{ delay: 3000 }}
+        mousewheel={{ invert: true }}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        preloadImages={false}
+        loop={true}
+        watchSlidesProgress={true}
+        speed={1000}
+        parallax={true}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+        style={{ transform: `rotate(-90deg)` }}
+      >
+        <SwiperSlide>
+          <div className="item-child-container">
+            <div className="item-subchild">
+              <img
+                data-swiper-parallax={-200}
+                src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/3/28/0328192-2590-FCBD-5A018FCFA66A0706.jpg"
+                alt="sunrise"
+              />
+            </div>
+            <div className="item-subchild">
+              <img
+                data-swiper-parallax={-200}
+                src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
+                alt="sunrise"
+              />
+            </div>
           </div>
-          <div className="item-subchild">
-            <img
-              data-swiper-parallax={-200}
-              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
-              alt="sunrise"
-            />
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="item-child-container">
+            <div className="item-subchild">
+              <img
+                data-swiper-parallax={-200}
+                src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/3/28/0328192-2590-FCBD-5A018FCFA66A0706.jpg"
+                alt="sunrise"
+              />
+            </div>
+            <div className="item-subchild">
+              <img
+                data-swiper-parallax={-200}
+                src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
+                alt="sunrise"
+              />
+            </div>
           </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="item-child-container">
-          <div className="item-subchild">
-            <img
-              data-swiper-parallax={-200}
-              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/3/28/0328192-2590-FCBD-5A018FCFA66A0706.jpg"
-              alt="sunrise"
-            />
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="item-child-container">
+            <div className="item-subchild">
+              <img
+                data-swiper-parallax={-200}
+                src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/3/28/0328192-2590-FCBD-5A018FCFA66A0706.jpg"
+                alt="sunrise"
+              />
+            </div>
+            <div className="item-subchild">
+              <img
+                data-swiper-parallax={-200}
+                src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
+                alt="sunrise"
+              />
+            </div>
           </div>
-          <div className="item-subchild">
-            <img
-              data-swiper-parallax={-200}
-              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
-              alt="sunrise"
-            />
-          </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="item-child-container">
-          <div className="item-subchild">
-            <img
-              data-swiper-parallax={-200}
-              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/3/28/0328192-2590-FCBD-5A018FCFA66A0706.jpg"
-              alt="sunrise"
-            />
-          </div>
-          <div className="item-subchild">
-            <img
-              data-swiper-parallax={-200}
-              src="https://d2rdb56lmj4es2.cloudfront.net/data/photo/2019/2/18/0218196-2590-FCBD-5AA19539BBCE5DDF.jpg"
-              alt="sunrise"
-            />
-          </div>
-        </div>
-      </SwiperSlide>
-    </Swiper>
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 };
 export default Slider;
