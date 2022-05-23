814c687e00d8406faf48dee727ebc07393d10292
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2022-05-23 12:07:27 +0530

    fix slides per view
    Make Slides per view according the window size change

--- pages/slider.js
+++ pages/slider.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 // import required modules
@@ -21,57 +21,112 @@ import "swiper/css/parallax";
 import "swiper/css/navigation";
 
 export default function slider() {
+  const [width, setWidth] = useState(); // default width, detect on server.
 
-  function showSubchildText(){
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
 
-  }
-  function hideSubchildText(){
-
-  }
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [handleResize]);
   return (
     <>
-      <Swiper
-        modules={[Pagination, A11y, Autoplay, Mousewheel, Parallax, Navigation]}
-        navigation={true}
-        spaceBetween={10}
-        slidesPerView={3}
-        autoplay={{ delay: 3000 }}
-        mousewheel={{ invert: true }}
-        pagination={{
-          clickable: true,
-          dynamicBullets: true,
-        }}
-        preloadImages={false}
-        loop={true}
-        watchSlidesProgress={true}
-        speed={1000}
-        parallax={true}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-      >
-        <SwiperSlide>
-          <div
-            className="item-child-container"
-            onMouseEnter={showSubchildText}
-            onMouseLeave={hideSubchildText}
-          >
-            <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
-            <div className="item-subchild">Hi</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="item-child-container">
-            <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
-            <div className="item-subchild">Hi</div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="item-child-container">
-            <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
-            <div className="item-subchild">Hi</div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+      {width < 767 ? (
+        <Swiper
+          modules={[
+            Pagination,
+            A11y,
+            Autoplay,
+            Mousewheel,
+            Parallax,
+            Navigation,
+          ]}
+          navigation={true}
+          spaceBetween={10}
+          slidesPerView={2}
+          autoplay={{ delay: 3000 }}
+          mousewheel={{ invert: true }}
+          pagination={{
+            clickable: true,
+            dynamicBullets: true,
+          }}
+          preloadImages={false}
+          loop={true}
+          watchSlidesProgress={true}
+          speed={1000}
+          parallax={true}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide>
+            <div className="item-child-container">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+              <div className="item-subchild">Hi</div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="item-child-container">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+              <div className="item-subchild">Hi</div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="item-child-container">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+              <div className="item-subchild">Hi</div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      ) : (
+        <Swiper
+          modules={[
+            Pagination,
+            A11y,
+            Autoplay,
+            Mousewheel,
+            Parallax,
+            Navigation,
+          ]}
+          navigation={true}
+          spaceBetween={10}
+          slidesPerView={3}
+          autoplay={{ delay: 3000 }}
+          mousewheel={{ invert: true }}
+          pagination={{
+            clickable: true,
+            dynamicBullets: true,
+          }}
+          preloadImages={false}
+          loop={true}
+          watchSlidesProgress={true}
+          speed={1000}
+          parallax={true}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide>
+            <div className="item-child-container">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+              <div className="item-subchild">Hi</div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="item-child-container">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+              <div className="item-subchild">Hi</div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="item-child-container">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+              <div className="item-subchild">Hi</div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      )}
     </>
   );
 }
