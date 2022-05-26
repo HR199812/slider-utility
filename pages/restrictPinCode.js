b32569f1ea50ea82451c420e81b6e3c7d4da0694
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2022-05-26 20:52:37 +0530

    Add XML2Js, USPS City/State Lookup

--- pages/restrictPinCode.js
+++ pages/restrictPinCode.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
+import { parseString } from "xml2js";
+
 export default function restrictpincode() {
   const initialState = { city: "", state: "" };
   const [pincode, setPincode] = useState(0);
@@ -7,7 +9,31 @@ export default function restrictpincode() {
   function GetStateCity() {
     console.log(pincode);
     if (pincode.toString().length === 5 && pincode >= 0 && pincode <= 99950) {
-      alert("Hi");
+      let fetchXML = `
+        <CityStateLookupRequest USERID="535HOME02105">
+          <ZipCode>
+            <Zip5>${pincode}</Zip5>
+          </ZipCode>
+        </CityStateLookupRequest>`;
+      let cityState = axios
+        .get(
+          `http://production.shippingapis.com/ShippingAPI.dll?API=CityStateLookup&xml=${encodeURIComponent(
+            fetchXML
+          )}`
+        )
+        .then((res) => {
+          parseString(res.data, function (err, result) {
+            console.dir(result.CityStateLookupResponse.ZipCode[0].City[0]);
+            console.dir(result.CityStateLookupResponse.ZipCode[0].State[0]);
+            initialState.city =
+              result.CityStateLookupResponse.ZipCode[0].City[0];
+            initialState.state =
+              result.CityStateLookupResponse.ZipCode[0].State[0];
+
+            setInit(initialState);
+          });
+          //   console.log(res.data);
+        });
     } else {
       alert("Pincode Length should be 5");
     }
@@ -22,8 +48,8 @@ export default function restrictpincode() {
           placeholder="Enter Pin Code"
           onChange={(e) => setPincode(e.target.value)}
         />
-        <input type="text" placeholder="City" readOnly />
-        <input type="text" placeholder="State" readOnly />
+        <input type="text" placeholder="City" readOnly value={init.city} />
+        <input type="text" placeholder="State" readOnly value={init.state} />
         <button onClick={GetStateCity}>Search</button>
       </div>
     </>
