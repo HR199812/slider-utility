564179c9c2b8eb2772d14233eb0be6c88f3657d9
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2022-05-29 16:00:21 +0530

    Add Mobile Image Swiper

---
+++ pages/mobileslider.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import required modules
+import {
+  Pagination,
+  A11y,
+  Autoplay,
+  Mousewheel,
+  Parallax,
+  Navigation,
+} from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
+import "swiper/css/mousewheel";
+import "swiper/css/parallax";
+import "swiper/css/navigation";
+
+export default function mobileslider() {
+  const [width, setWidth] = useState(); // default width, detect on server.
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [handleResize]);
+  return (
+    <>
+      <div className="nav-icon">
+        <img
+          src="https://api.exhibitacollection.com/wp-content/uploads/2021/08/nav-icon.png"
+          width="10%"
+          className="mob-slider-nav-icon"
+        />
+      </div>
+      <Swiper
+        style={{ height: `100vh !important` }}
+        modules={[A11y, Autoplay, Mousewheel, Parallax]}
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{ delay: 3000 }}
+        mousewheel={{ invert: true }}
+        preloadImages={false}
+        loop={true}
+        watchSlidesProgress={true}
+        speed={1000}
+        parallax={true}
+      >
+        <SwiperSlide>
+          <div>
+            <div className="item-subchild-one">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+            </div>
+            <div className="item-subchild-two">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <div className="item-subchild-one">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+            </div>
+            <div className="item-subchild-two">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <div className="item-subchild-one">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+            </div>
+            <div className="item-subchild-two">
+              <img src="https://api.exhibitacollection.com/wp-content/uploads/2021/12/croc-on-crc-wallet-product-D.jpg" />
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <div className="mob-slider-text-title">
+        <p class="artwork_mobile_slider-title" style={{ color: "#ffffff" }}>
+          <a href="#">
+            COLLABORATIONS WITH ARTISTS
+            <span class="artwork-title-second">DISCOVER THE ARTISTS</span>
+          </a>
+        </p>
+      </div>
+    </>
+  );
+}
